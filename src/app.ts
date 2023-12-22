7bf39f0efd1ad8af9ac08964d0e92b133fac4949
JECR9421 <ch2jsr@hotmail.com>
2023-12-22 10:56:14 -0600

    Upload from queue

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,10 @@ import express, { Application, Request, Response } from 'express';
 import { uploadFiles, downloadFiles } from './controllers/s3.controller';
 import multer from 'multer';
 import bodyParser from 'body-parser';
+import amqp from "amqplib";
 const cors = require('cors');
 
-
+// Server  expresss
 const app: Application = express();
 
 const PORT: number = 3001;
@@ -29,3 +30,36 @@ app.listen(PORT, (): void => {
     console.log('env', process.env);
     console.log('SERVER IS UP ON PORT:', PORT);
 });
+
+// Upload queue
+const queue = "upload_queue";
+const handleReceiver = async () => {
+    try {
+        const connection = await amqp.connect("amqp://user:bitnami@209.126.7.137:5008");
+        const channel = await connection.createChannel();
+    
+        process.once("SIGINT", async () => {
+          await channel.close();
+          await connection.close();
+        });
+    
+        await channel.assertQueue(queue, { durable: false });
+        await channel.consume(
+          queue,
+          (message:any) => {
+            if (message) {
+              console.log(
+                " [x] Received '%s'",
+                JSON.parse(message.content.toString())
+              );
+            }
+          },
+          { noAck: true }
+        );
+    
+        console.log(" [*] Waiting for messages. To exit press CTRL+C");
+      } catch (err) {
+        console.warn(err);
+      }
+};
+handleReceiver();
